622c369325119613abe5557cda060855dc14505a
Nate Barrett <45924215+natebarrett98@users.noreply.github.com>
2020-01-18 08:48:36 -0800

    Create Data Visualization with Matplotlib Practice.py

---
+++ Data Visualization with Matplotlib Practice.py
@@ -0,0 +1,34 @@
+'''
+DSU Data Visualization Practice
+
+Here are a series of links to recieve a dataset from, I want you to extract and read in a dataset into your module
+(You do not need to use the links if you already have a dtatset)
+
+
+Links :
+
+[ISLR Datasets, Good fundamental Datasets to use for analysis]
+http://faculty.marshall.usc.edu/gareth-james/ISL/data.html
+
+[Data.gov Health Datasets, Modern Health Datasets used for Modern Statistical Applications]
+https://catalog.data.gov/dataset?_organization_limit=0&organization=hhs-gov#topic=health_navigation
+
+[Kaggle Datasets, Good Archieve of Datasets to use for Analysis]
+https://www.kaggle.com/datasets?fileType=csv
+
+
+1)
+-Create a 4-subpplot figure, each consisting of a different type of plot
+-Line,Bar,Histogram, Scatterplot, etc...
+-Make sure to include x, and y labels, as well as a title.
+-Make each plot a specific linestyle, marker and color when applicable (when are these applicable)
+
+
+2)
+-Create a multiline graph that depcits 4 types of unique variables.
+-Make sure to include a legend, title, and labels
+-Each line should have a different marker, linestyle, and color
+'''
+
+
+
